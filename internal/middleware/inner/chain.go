f8bb5de106412a0da8c4f7889ff9cf86c7df833e
Peter Gillich <pgillich@gmail.com>
2023-10-26 12:58:42 +0200

    Fix internal mw first item

--- internal/middleware/inner/chain.go
+++ internal/middleware/inner/chain.go
@@ -12,7 +12,7 @@ type InternalMiddleware func(next InternalMiddlewareFn) InternalMiddlewareFn
 func InternalMiddlewareChain(mws ...InternalMiddleware) InternalMiddleware {
 	return func(next InternalMiddlewareFn) InternalMiddlewareFn {
 		fn := next
-		for mw := len(mws) - 1; mw > 0; mw-- {
+		for mw := len(mws) - 1; mw >= 0; mw-- {
 			fn = mws[mw](fn)
 		}
 
